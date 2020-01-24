659da15e40d515ed0ad978660db2a480d7c0638a
Jenny Ardrey <jenny@ardrey.co.uk>
2020-01-24 22:03:22 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.status(200).json({ message: 'Bonjour, Terre!' });
 });
 
 module.exports = app;
